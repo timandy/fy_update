8f63bef2756d8c6e70ecd65adef3c9147842a9ac
TimAndy <xuchonglei@126.com>
2016-09-25 17:35:31 +0800

    添加结束进程事件数据

---
+++ WfyUpdate/Net/KillProgressChangedEvent.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel;
+
+namespace WfyUpdate.Net
+{
+    /// <summary>
+    /// 结束进程进度报告事件类型
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    public delegate void KillProgressChangedEventHandler(object sender, KillProgressChangedEventArgs e);
+
+    /// <summary>
+    /// 结束进程进度报告事件数据
+    /// </summary>
+    public class KillProgressChangedEventArgs : ProgressChangedEventArgs
+    {
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        /// <param name="progressPercentage">进度百分比</param>
+        /// <param name="userState">用户数据</param>
+        public KillProgressChangedEventArgs(int progressPercentage, object userState)
+            : base(progressPercentage, userState)
+        {
+        }
+    }
+}

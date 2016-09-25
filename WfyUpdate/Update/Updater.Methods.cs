ca100bcbc77c5fd6ca17abb63d9c1dddb92b7fe8
TimAndy <xuchonglei@126.com>
2016-09-25 12:58:28 +0800

    改为异步解压

--- WfyUpdate/Update/Updater.Methods.cs
+++ WfyUpdate/Update/Updater.Methods.cs
@@ -1,8 +1,8 @@
 ﻿using System;
-using System.ComponentModel;
 using System.Net;
 using WfyUpdate.Config;
 using WfyUpdate.Model;
+using WfyUpdate.Net;
 using WfyUpdate.Update.Event;
 using WfyUpdate.Util;
 
@@ -48,7 +48,7 @@ namespace WfyUpdate.Update
         /// <summary>
         /// 异步检查开始
         /// </summary>
-        protected virtual void BeginCheck()
+        protected virtual void CheckAsync()
         {
             this.DisposeAvaliables();
             HostConfig.Refresh();
@@ -75,7 +75,7 @@ namespace WfyUpdate.Update
         /// 异步检查结束
         /// </summary>
         /// <param name="e">结果</param>
-        protected virtual void EndCheck(DownloadStringCompletedEventArgs e)
+        protected virtual void CheckCompleted(DownloadStringCompletedEventArgs e)
         {
             //用户取消
             if (e.Cancelled)
@@ -111,13 +111,13 @@ namespace WfyUpdate.Update
             this.m_Avaliables = avaliables.GetEnumerator();
             //开始下载
             this.OnUpdateStarted(new UpdateStartedEventArgs(avaliables));
-            this.BeginDownload();
+            this.DownloadAsync();
         }
 
         /// <summary>
         /// 异步下载开始
         /// </summary>
-        protected virtual void BeginDownload()
+        protected virtual void DownloadAsync()
         {
             //验证
             if (this.m_Avaliables == null)
@@ -141,13 +141,13 @@ namespace WfyUpdate.Update
         /// 异步下载结束
         /// </summary>
         /// <param name="e">结果</param>
-        protected virtual void EndDownload(DownloadDataCompletedEventArgs e)
+        protected virtual void DownloadCompleted(DownloadDataCompletedEventArgs e)
         {
             //验证
             UpdatePackage package = e.UserState as UpdatePackage;
             if (package == null)
             {
-                this.OnError(new ErrorEventArgs("无效的操作。"));
+                this.OnError(new ErrorEventArgs("无效的下载操作。"));
                 return;
             }
             //用户取消
@@ -162,37 +162,47 @@ namespace WfyUpdate.Update
                 this.OnError(new ErrorEventArgs("下载 {0} 失败:{1}。", package.FileName, e.Error.Message.TrimEnd(PERIOD)));
                 return;
             }
-            //解压
-            try
-            {
-                this.OnNotify(new NotifyEventArgs("正在解压 {0}。", package.FileName));
-                CompressUtil.Decompress(e.Result, HostConfig.ExecutableDirectory, HostConfig.ExecutableName);
-            }
-            catch (Exception exp)
-            {
-                this.OnError(new ErrorEventArgs("解压 {0} 失败:{1}。", package.FileName, exp.Message.TrimEnd(PERIOD)));
-                return;
-            }
-            //继续下一个
-            this.BeginDownload();
+            this.DecompressAsync(e.Result, package);
         }
 
         /// <summary>
         /// 异步解压开始
         /// </summary>
-        /// <param name="buffer">要解压的数据</param>
-        protected virtual void BeginDecompress(byte[] buffer)
+        /// <param name="data">要解压的数据</param>
+        protected virtual void DecompressAsync(byte[] data, UpdatePackage package)
         {
-            //TODO
+            //解压
+            this.OnNotify(new NotifyEventArgs("正在解压 {0}。", package.FileName));
+            this.m_WebClient.DecompressDataAsync(data, HostConfig.ExecutableName, HostConfig.ExecutableDirectory, package);
         }
 
         /// <summary>
         /// 异步解压结束
         /// </summary>
         /// <param name="e">结果</param>
-        protected virtual void EndDecompress(AsyncCompletedEventArgs e)
+        protected virtual void DecompressCompleted(DecompressDataCompletedEventArgs e)
         {
-            //TODO
+            //验证
+            UpdatePackage package = e.UserState as UpdatePackage;
+            if (package == null)
+            {
+                this.OnError(new ErrorEventArgs("无效的解压操作。"));
+                return;
+            }
+            //用户取消
+            if (e.Cancelled)
+            {
+                this.OnNotify(new NotifyEventArgs("已取消更新。"));
+                return;
+            }
+            //出错
+            if (e.Error != null)
+            {
+                this.OnError(new ErrorEventArgs("解压 {0} 失败:{1}。", package.FileName, e.Error.Message.TrimEnd(PERIOD)));
+                return;
+            }
+            //继续下一个
+            this.DownloadAsync();
         }
     }
 }

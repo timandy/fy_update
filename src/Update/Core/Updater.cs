4199461fd1c3e62411f385ae89e8d58a15a1dbf1
TimAndy <xuchonglei@126.com>
2016-10-08 16:09:39 +0800

    Updater 增加检查事件

--- src/Update/Core/Updater.cs
+++ src/Update/Core/Updater.cs
@@ -11,10 +11,10 @@ namespace Update.Core
     /// </summary>
     public partial class Updater : Disposable, IUpdater
     {
-        private static readonly char[] PERIOD = { '.', '。' };      //句号
-        private static readonly string PACKAGES = "packages.txt";   //服务端配置文件名
-        private UpdateClient m_Client;                              //客户端
-        private IEnumerator<IPackage> m_Avaliables;                 //更新枚举器
+        private static readonly char[] PERIOD = { '.', '。' };          //句号
+        private static readonly string PACKAGES = "packages.txt";       //服务端配置文件名
+        private UpdateClient m_Client;                                  //客户端
+        private IEnumerator<IPackage> m_Avaliables;                     //更新枚举器
 
         /// <summary>
         /// 构造函数
@@ -23,12 +23,12 @@ namespace Update.Core
         {
             this.m_Client = new UpdateClient { Encoding = System.Text.Encoding.UTF8 };
             this.m_Client.KillProgressChanged += (sender, e) => this.OnProgress(new ProgressEventArgs(e.ProgressPercentage));
-            this.m_Client.KillProcessCompleted += (sender, e) => this.KillCompleted(e);
+            this.m_Client.KillProcessCompleted += (sender, e) => this.ClientKillCompleted(e);
             this.m_Client.DownloadProgressChanged += (sender, e) => this.OnProgress(new ProgressEventArgs(e.ProgressPercentage));
-            this.m_Client.DownloadStringCompleted += (sender, e) => this.CheckCompleted(e);
-            this.m_Client.DownloadDataCompleted += (sender, e) => this.DownloadCompleted(e);
+            this.m_Client.DownloadStringCompleted += (sender, e) => this.ClientCheckCompleted(e);
+            this.m_Client.DownloadDataCompleted += (sender, e) => this.ClientDownloadCompleted(e);
             this.m_Client.DecompressProgressChanged += (sender, e) => this.OnProgress(new ProgressEventArgs(e.ProgressPercentage));
-            this.m_Client.DecompressDataCompleted += (sender, e) => this.DecompressCompleted(e);
+            this.m_Client.DecompressDataCompleted += (sender, e) => this.ClientDecompressCompleted(e);
         }
     }
 }

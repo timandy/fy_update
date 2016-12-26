4e47a47db856b08e5d1249acd2bf21ba9278da11
TimAndy <xuchonglei@126.com>
2016-12-26 17:02:57 +0800

    功能调整

--- src/Update/Util/UpdateUtil.cs
+++ src/Update/Util/UpdateUtil.cs
@@ -1,6 +1,4 @@
-﻿using Update.Config;
-
-namespace Update.Util
+﻿namespace Update.Util
 {
     /// <summary>
     /// 更新工具类
@@ -8,38 +6,17 @@ namespace Update.Util
     public static class UpdateUtil
     {
         /// <summary>
-        /// 检查更新,不允许取消更新
-        /// </summary>
-        /// <returns>true 表示程序应继续运行,否则程序应退出</returns>
-        public static bool CheckUpdate()
-        {
-            return CheckUpdate(false);
-        }
-
-        /// <summary>
         /// 检查更新,可指定是否允许取消更新
         /// </summary>
         /// <param name="allowCancel">是否允许取消更新</param>
         /// <returns>true 表示程序应继续运行,否则程序应退出</returns>
-        public static bool CheckUpdate(bool allowCancel)
+        public static bool CheckUpdate(bool allowCancel = false)
         {
-            //声明
-            bool completed;
-            bool uptodate;
             //检查更新
-            using (FrmUpdate dialog = new FrmUpdate { CheckMode = true })
+            using (FrmUpdate dialog = new FrmUpdate())
             {
-                dialog.ShowDialog();
-                completed = dialog.CheckCompleted;
-                uptodate = dialog.Uptodate;
+                return dialog.CheckUpdate(allowCancel);
             }
-            //用户取消或发生错误
-            if (!completed)
-                return allowCancel;
-            //发现新版本
-            if (!uptodate)
-                ProcessUtil.Start(AppConfig.AssemblyPath, HostConfig.ExecutablePath);
-            return uptodate;
         }
     }
 }
